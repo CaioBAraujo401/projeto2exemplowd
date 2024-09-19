7fd763d4f1bdfec8f669bea307a8c978b1db021e
CaioAraujo <sapoalho@gmail.com>
2024-09-19 11:14:54 -0300

    criando as variaveis indefinidas e vazias

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
 
 // DECLAARANDO AS VARIAVEIS 
 
+
+//declaracao indefinida
+let nome3;
+console.log(nome);
+
+//declaração vazia
+let disciplina =null;
+console.log(disciplina);
+
+//declaração definida ou com valor
 var nome="fiap";
-console.log(nome)
+console.log(nome);
 
 let nome1="cidade";
-console.log(nome1)
+console.log(nome1);
 
-const nome2="javascript"
-console.log(nome2)
+const nome2="javascript";
+console.log(nome2);
+
+
