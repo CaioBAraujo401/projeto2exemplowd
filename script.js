7542911824924f4cec1bf5feef508ec5edff5e2b
CaioAraujo <sapoalho@gmail.com>
2024-09-19 11:23:20 -0300

    criando tipos de variaveis

--- script.js
+++ script.js
@@ -21,4 +21,24 @@ console.log(nome1);
 const nome2="javascript";
 console.log(nome2);
 
+//tipos de variáveis 
 
+//inteiro
+let idade = 18;
+console.log(idade)
+
+//float / com casas decimais 
+let valor=5.50;
+console.log(valor)
+
+//strings / de texto
+let texto="fiap"
+console.log(texto)
+
+//booleanos - true or false
+let aluno=true
+console.log(aluno)
+
+//array / conjunto de informações 
+let tecnologias=["java", "C#", "HTML", "CSS"]
+console.log(tecnologias)
